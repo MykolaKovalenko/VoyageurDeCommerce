3eafed095d1b8801af55616c62cf31e7dcd6a9b8
vahem92wordpress-collab <vahem92wordpress@gmail.com>
2026-03-31 10:00:02 +0200

    22fg
    gthyythy

--- voyageurCommerce/src/App.java
+++ voyageurCommerce/src/App.java
@@ -2,20 +2,20 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("===== TEST VOYAGEUR DE COMMERCE =====\n");
 
-        Graphe g1 = new Graphe();
-        g1.importer("voyageurCommerce/data/14_points.csv");
+        // Graphe g1 = new Graphe();
+        // g1.importer("voyageurCommerce/data/14_points.csv");
 
-        for(int i = 0; i < 10; i++) {
-            Graphe g2 = g1.glouton();
-            g2.afficher();
-            System.out.println("Cout du graphe : " + g2.cout());
-        }
+        // for(int i = 0; i < 10; i++) {
+        //     Graphe g2 = g1.glouton();
+        //     g2.afficher();
+        //     System.out.println("Cout du graphe : " + g2.cout());
+        // }
 
-        // Graphe g3 = new Graphe();
-        // g3.importer("voyageurCommerce/data/14_points.csv");
-        // g3.partiel(13);
-        // g3.afficher();
-        // System.out.println("Cout du graphe : " + g3.cout());
+        Graphe g3 = new Graphe();
+        g3.importer("voyageurCommerce/data/14_points_melanges.csv");
+        g3.partiel(2);
+        g3.afficher();
+        System.out.println("Cout du graphe : " + g3.cout());
 
         // Graphe g4 = new Graphe();
         // g4.importer("voyageurCommerce/data/80_points.csv");
